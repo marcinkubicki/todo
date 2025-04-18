daea11930b1b2f1bf62b9c7e1ddbd9e32988a2ef
marcin <marcin.kubicki32@gmail.com>
2025-04-18 19:59:57 +0200

    Add ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Todo } from "@/components/Todo";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
    return (
       <main className="h-full flex flex-col justify-center items-center p-8">
-         <Todo />
+         <ErrorBoundary fallback={<p>⚠️Something went wrong</p>}>
+            <Todo />
+         </ErrorBoundary>
       </main>
    )
 }
 
-export default App
+export default App;
